2f2ce73ae768ebabb2bcf38f9a9311762745aa2b
phusondx95 <frankvanphu@gmail.com>
2017-05-17 13:42:16 +0700

    Add password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,13 @@ class UsersController < ApplicationController
     @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
 
+  # def show
+  #	@user = User.find(params[:id])
+   # redirect_to root_url and return unless FILL_IN
+  #end
+
   def show
-  	@user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    @user = User.find(params[:id])
   end
 
   def new
